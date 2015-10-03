35f8df451c22d4d6326025db8a76ba8214c9d9d0
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-10-03 15:21:49 +0900

    add slide controller

--- js/comment.js
+++ js/comment.js
@@ -1,43 +1,129 @@
 $(function(){
 
-  var vueModel = new Vue({
+  var formVM = new Vue({
     el: '#comment_form',
     data: {
       comment: '',
-      size: '',
-      color: '',
-      type: ''
+      size: '36px',
+      color: '#000000',
+      speed: 10
     },
 
     created: function(){
-      this.comment = '（´・ω・｀）< まあ、押してみそ';
+      this.comment = '（´・ω・｀）< 押してみそ';
     },
 
     methods:{
 
       onSend: function(e){
-         messageDataStore.push({content: this.comment, type: 'text'});
+        messageDataStore.send({
+          content: this.comment,
+          size: this.size,
+          color: this.color,
+          speed: this.speed,
+          type: 'text'
+        });
+      },
+
+      onSendQuestion: function(e){
+        messageDataStore.send({
+          content: this.comment,
+          size: this.size,
+          color: this.color,
+          speed: this.speed,
+          type: 'question'
+        });
       },
 
       onSendLgtm: function(e){
         $.ajax({
           url: "http://lgtm.in/g"
         }).done(function(data){
-          var lgtmUrl = data.match(/(<.+ class="form-control" id="imageUrl" name="imageUrl">)/);
+          var lgtmUrl = data.match(/(<input type="text" value=.+ class="form-control" id="imageUrl" name="imageUrl">)/);
           if (0 < lgtmUrl.length) {
             var url = lgtmUrl[0]
                         .replace(/^(<input type="text" value=")/, '')
                         .replace(/(" class="form-control" id="imageUrl" name="imageUrl">)$/, '');
 
-            messageDataStore.push({content: url, type: 'image'});
+            messageDataStore.send({
+              content: url,
+              speed: 10,
+              size: '150',
+              type: 'image'
+            });
           } else {
             alert('画像がとれなかったよ・・・');
           }
         }).fail(function(data){
           alert('つながらないよ・・・');
         });
+      },
+
+      onSendFeelGood: function(e) {
+        messageDataStore.send({
+          content: './image/iineppoi.png',
+          speed: 10,
+          size: '50',
+          type: 'image'
+        });
+      },
+      onSendISee: function(e) {
+        messageDataStore.send({
+          content: 'φ(・ω・ ) < なるほど !',
+          speed: 10,
+          size: '36px',
+          color: '#f59200',
+          type: 'text'
+        });
+      },
+      onSendCantUnderstand: function(e) {
+        messageDataStore.send({
+          content: '( ﾟДﾟ) < なるほど、まったくわからん',
+          speed: 10,
+          size: '36px',
+          color: '#9525a8',
+          type: 'text'
+        });
       }
     }
   });
 
+  var questionVM = new Vue({
+    el: '#question_list',
+    data: {
+      questions: []
+    },
+    created: function(){
+      messageDataStore.stream().next(function(err, data) {
+        data.map(function(v) {
+          var sendDate = new Date(v.timestamp);
+          questionVM.questions.push({
+            text: v.value.text,
+            time: sendDate.getFullYear() + '/' +
+                  ('0' + (sendDate.getMonth() + 1)).slice(-2) + '/' +
+                  ('0' + sendDate.getDate()).slice(-2) + ' ' +
+                  ('0' + sendDate.getHours()).slice(-2) + ':' +
+                  ('0' + sendDate.getMinutes()).slice(-2) + ':' +
+                  ('0' + sendDate.getSeconds()).slice(-2),
+            page: v.value.page
+          });
+        });
+      });
+    },
+  });
+  messageDataStore.on('push', function(pushed) {
+
+    var sendDate = new Date(pushed.timestamp);
+
+    questionVM.questions.push({
+      text: pushed.value.text,
+      time: sendDate.getFullYear() + '/' +
+            ('0' + (sendDate.getMonth() + 1)).slice(-2) + '/' +
+            ('0' + sendDate.getDate()).slice(-2) + ' ' +
+            ('0' + sendDate.getHours()).slice(-2) + ':' +
+            ('0' + sendDate.getMinutes()).slice(-2) + ':' +
+            ('0' + sendDate.getSeconds()).slice(-2),
+      page: pushed.value.page
+    });
+  });
 });
--- js/index.js
+++ js/index.js
@@ -13,17 +13,16 @@ $(function(){
     // commentAreaに追加されたcommentを取得
     var el = mutationRecords[0].addedNodes[0];
 
-
     // 初期表示時の位置とアニメーションのスタイルを設定
     el.style.right = '-' + el.offsetWidth + 'px';
     el.style.top = Math.floor( Math.random() * 90 ) + '%';
     el.style.webkitTransform = 'translate(-' + (commentArea.offsetWidth * 2) + 'px, 0px)';
-    el.style.webkitTransitionDuration = '10s';
+    el.style.webkitTransitionDuration = el.speed + 's';
 
     // アニメーションが終わったら消す
     setTimeout(function() {
       commentArea.removeChild(el);
-    }, 10000);
+    }, el.speed * 1000);
 
   });
 
@@ -31,15 +30,16 @@ $(function(){
   mo.observe(commentArea, {childList:true, attributes: false, attributeOldValue: false});
 
   // MilkCocoaからコメントを受け取った時のコールバック
-  messageDataStore.on('push', function(pushed) {
+  messageDataStore.on('send', function(pushed) {
 
     // コメントを表示するElementを作成
     var el = document.createElement('div');
     el.className = 'comment';
 
     if(pushed.value.type === 'image' || pushed.value.content.toLowerCase().search(/^((http|https):\/\/.+(\.jpg|\.gif|\.png))/) === 0) {
-      var image = new Image(150);
+      var image = new Image(pushed.value.size);
       image.onload = function(e){
+        el.speed = pushed.value.speed;
         el.appendChild(this);
         commentArea.appendChild(el);
       };
@@ -51,10 +51,18 @@ $(function(){
           return escapeHtml(v);
         })
         .join('<br />');
+      el.style.color = pushed.value.color;
+      el.style['font-size'] = pushed.value.size;
+      el.speed = pushed.value.speed;
       commentArea.appendChild(el);
-    }
 
-    console.dir(pushed);
+      if(pushed.value.type === 'question') {
+        messageDataStore.push({
+          text: pushed.value.content,
+          page: window.location.hash
+        });
+      }
+    }
   });
 
   // HTMLエスケープ用
