4bf3486dfdda026a237ae28c41fab1185c8068ee
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-10-14 09:07:50 +0900

    refactor js

--- js/comment.js
+++ js/comment.js
@@ -1,5 +1,8 @@
 $(function(){
 
+  /**
+   * コメント投稿用VueModel
+   */
   var formVM = new Vue({
     el: '#comment_form',
     data: {
@@ -15,6 +18,9 @@ $(function(){
 
     methods:{
 
+      /**
+       * 投稿ボタン押下時
+       */
       onSend: function(e){
         messageDataStore.send({
           content: this.comment,
@@ -25,6 +31,9 @@ $(function(){
         });
       },
 
+      /**
+       * 質問投稿ボタン押下時
+       */
       onSendQuestion: function(e){
         messageDataStore.push({
           content: this.comment,
@@ -35,6 +44,9 @@ $(function(){
         });
       },
 
+      /**
+       * LGTMボタン押下時
+       */
       onSendLgtm: function(e){
         $.ajax({
           url: "http://lgtm.in/g"
@@ -59,6 +71,9 @@ $(function(){
         });
       },
 
+      /**
+       * いいねボタン押下時
+       */
       onSendFeelGood: function(e) {
         messageDataStore.send({
           content: './image/iineppoi.png',
@@ -67,6 +82,10 @@ $(function(){
           type: 'image'
         });
       },
+
+      /**
+       * なるほどボタン押下時
+       */
       onSendISee: function(e) {
         messageDataStore.send({
           content: 'φ(・ω・ ) < なるほど !',
@@ -76,6 +95,10 @@ $(function(){
           type: 'text'
         });
       },
+
+      /**
+       * わからんボタン押下時
+       */
       onSendCantUnderstand: function(e) {
         messageDataStore.send({
           content: '( ﾟДﾟ) < なるほど、まったくわからん',
@@ -88,41 +111,52 @@ $(function(){
     }
   });
 
+  /**
+   * 質問一覧VueModel
+   */
   var questionVM = new Vue({
     el: '#question_list',
     data: {
       questions: []
     },
-    created: function(){
+    created: function() {
+
+      // 画面表示時に投稿済の質問の一覧を取得してすべて表示する
       messageDataStore.stream().next(function(err, data) {
         data.map(function(v) {
-          var sendDate = new Date(v.timestamp);
           questionVM.questions.push({
             text: v.value.content,
-            time: sendDate.getFullYear() + '/' +
-                  ('0' + (sendDate.getMonth() + 1)).slice(-2) + '/' +
-                  ('0' + sendDate.getDate()).slice(-2) + ' ' +
-                  ('0' + sendDate.getHours()).slice(-2) + ':' +
-                  ('0' + sendDate.getMinutes()).slice(-2) + ':' +
-                  ('0' + sendDate.getSeconds()).slice(-2)
+            time: formatDateToString(new Date(v.timestamp))
           });
         });
       });
     }
   });
 
+  /**
+   * 質問投稿時のイベントハンドラ
+   */
   messageDataStore.on('push', function(pushed) {
 
-    var sendDate = new Date(pushed.timestamp);
-
+    // 質問の投稿を受け取ったら一覧に追加表示する
     questionVM.questions.push({
       text: pushed.value.content,
-      time: sendDate.getFullYear() + '/' +
-            ('0' + (sendDate.getMonth() + 1)).slice(-2) + '/' +
-            ('0' + sendDate.getDate()).slice(-2) + ' ' +
-            ('0' + sendDate.getHours()).slice(-2) + ':' +
-            ('0' + sendDate.getMinutes()).slice(-2) + ':' +
-            ('0' + sendDate.getSeconds()).slice(-2)
+      time: formatDateToString(new Date(pushed.timestamp))
     });
   });
+
+  /**
+   * 日付をyyyyMMdd HH:mm:ss の形式に変換する
+   *
+   * @param  {object} date Dateオブジェクト
+   * @return {string}      フォーマット後の文字列
+   */
+  var formatDateToString = function(date) {
+    return date.getFullYear() + '/' +
+          ('0' + (date.getMonth() + 1)).slice(-2) + '/' +
+          ('0' + date.getDate()).slice(-2) + ' ' +
+          ('0' + date.getHours()).slice(-2) + ':' +
+          ('0' + date.getMinutes()).slice(-2) + ':' +
+          ('0' + date.getSeconds()).slice(-2);
+  };
 });
